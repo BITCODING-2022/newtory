45781d674923d3c3dc4b12f348ecafc8bd5b0cfd
suddiyo <suddii01@sju.ac.kr>
2022-02-10 23:59:42 +0900

    pocket init

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,28 @@
 import React, {Component} from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content'
+import Subject from './components/Subject'
 import './App.css';
 
+
 class App extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            subject:{title:'Pocket', sub:'Hello Pocket!'}
+        }
+    }
     render() {
         return (
-            <h1>Hello Pocket!</h1>
+            <div>
+                <Subject
+                title={this.state.subject.title}
+                sub={this.state.subject.sub}>
+                </Subject>
+                <TOC></TOC>
+                <Content title="HTML" desc="HTML is HyperText Markup Language.">
+                </Content>
+            </div>
         );
     }
 }
